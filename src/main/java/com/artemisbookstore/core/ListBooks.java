a7005b5123d019eb9f760dfdc8ca1ed90eb90852
gloucklegnou <philippe.genoud@gmail.com>
2019-10-21 16:26:18 +0200

    suppression imports inutiles

--- src/main/java/com/artemisbookstore/core/ListBooks.java
+++ src/main/java/com/artemisbookstore/core/ListBooks.java
@@ -24,9 +24,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.FileManager;
-import org.apache.jena.vocabulary.DCTerms;
 
 /**
  *
